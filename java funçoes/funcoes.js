6b751577111833a05339d323bed542028e1956da
Helber10 <helber.silv@gmail.com>
2023-10-30 14:42:45 -0300

    menu

--- java funçoes/funcoes.js
+++ java funçoes/funcoes.js
@@ -7,3 +7,17 @@ function toggleMenu() {
 }
 btnMenu.addEventListener("click", toggleMenu);
 btnMenu.addEventListener("touchstart", toggleMenu);
+
+//simula login///
+
+
+function tipoLogin() {
+  const username = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "@email" && password === "1234") {
+    window.location.href = "esporte.html";
+  } else {
+    alert("Você não Esta logado");
+  }
+}
