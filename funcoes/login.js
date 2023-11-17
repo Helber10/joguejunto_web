a47424ac6d74f72fa07ae4cdcd63cf09ccc2b626
Helber10 <helber.silv@gmail.com>
2023-11-17 19:46:09 -0300

    atulizaçao

---
+++ funcoes/login.js
@@ -0,0 +1,27 @@
+
+
+//simula login///
+
+
+function tipoLogin() {
+  const username = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "@email" && password === "1234") {
+    window.location.href = "escolhaesporte.html";
+     alert("Você Esta logado");
+  } else {
+    alert("Você não Esta logado");
+  }
+}
+//fim///
+
+ // validar cadastro//
+ ///fim///
+
+ ///organiza partidas///
+ 
+
+ ///fimm/////
+
+ 
